e20e243b226f3f767ff61a74f0da306d1db6b344
Zachary Trank-Zelewicz <trank.zachary@gmail.com>
2019-08-29 11:56:45 -0400

    Adds Exports

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './public/auth-service/public';
+export * from './public/character-service/public';
+export * from './public/environment-service/public';
+export * from './public/player-service/public';
+
+import { Bind as ServiceLayerBind } from './Bindings';
+export { ServiceLayerBind }
