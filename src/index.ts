9acb19f2dddac5a918f291562bb6790b36961751
Zachary Trank-Zelewicz <trank.zachary@gmail.com>
2019-08-30 10:38:37 -0400

    Updates exports

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './public/auth-service/public';
 export * from './public/character-service/public';
 export * from './public/environment-service/public';
 export * from './public/player-service/public';
+export * from './public/media-service/public';
 
 export { ServiceLayerModule } from './Service.Layer.Module';
